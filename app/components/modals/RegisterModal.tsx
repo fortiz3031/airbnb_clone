d1d9aa2f99fd0d355659fc2b37bbeb53d5733b2d
aressss1 <codewithfaizi@gmail.com>
2023-12-06 10:57:24 +0500

    f-

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -106,12 +106,6 @@ const RegisterModal = () => {
         icon={FcGoogle}
         onClick={() => signIn('google')}
       />
-      <Button 
-        outline
-        label="Continue With Github"
-        icon={AiFillGithub}
-        onClick={() => signIn('github')}
-      />
 
       <div className="text-neutral-500 text-center mt-4 font-light ">
         <div className="justify-center flex flex-row items-center gap-2">
